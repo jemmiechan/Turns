d572c5287c30c7d4a79957e0d6b7ece23a5404bb
Jemmie Chan <jemmiechan@Jemmies-MacBook-Pro.local>
2012-05-09 23:53:49 +0800

    Initial Commit

---
+++ Turns/TurnsDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  TurnsDetailViewController.h
+//  Turns
+//
+//  Created by Jemmie Lou A Chan on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TurnsDetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ Turns/TurnsMasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  TurnsMasterViewController.h
+//  Turns
+//
+//  Created by Jemmie Lou A Chan on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface TurnsMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
